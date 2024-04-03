7c52d70fa30561a00bc615a967ba86b01951c1fa
Rafael Pignatari <rafaelpignatarifantinati@hotmail.com>
2024-04-03 19:12:59 -0300

    Loja - Atualização de pacote de navegação

--- loja/App.js
+++ loja/App.js
@@ -1,4 +1,5 @@
-import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './telas/Home/index';
 import CadCompra from './telas/CadCompra/index';
 import CadProduto from './telas/CadProduto/index';
@@ -6,20 +7,21 @@ import CadCategoria from './telas/CadCategoria/index';
 import VisVendas from './telas/VisVendas/index';
 import Carrinho from './telas/Carrinho/index';
 
-const Routes = createAppContainer(
-  createSwitchNavigator({
-    Home,
-    CadCompra,
-    CadProduto,
-    CadCategoria,
-    VisVendas,
-    Carrinho
-  })
-);
+const Stack = createNativeStackNavigator();
 
 export default function App() {
 
   return (
-      <Routes/>
+    <NavigationContainer>
+    <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen name="Home" component={Home} options={{ headerShown: false, animation: 'fade' }} />
+      <Stack.Screen name="CadCompra" component={CadCompra} options={{ headerShown: false, animation: 'fade' }} />
+      <Stack.Screen name="CadProduto" component={CadProduto} options={{ headerShown: false, animation: 'fade'}} />
+      <Stack.Screen name="CadCategoria" component={CadCategoria} options={{ headerShown: false, animation: 'fade' }} />
+      <Stack.Screen name="VisVendas" component={VisVendas} options={{ headerShown: false, animation: 'fade'}} />
+      <Stack.Screen name="Carrinho" component={Carrinho} options={{ headerShown: false, animation: 'fade' }} />
+
+    </Stack.Navigator>
+  </NavigationContainer>
   );
 }
